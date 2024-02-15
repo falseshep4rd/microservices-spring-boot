eb4d556a13360d3c618bfccbb1cbea481f6dd40f
saras <sarazzz123@gmail.com>
2024-02-15 19:17:30 +0200

    updated inventory-service

--- inventory-service/src/main/java/com/shopproject/inventoryservice/InventoryServiceApplication.java
+++ inventory-service/src/main/java/com/shopproject/inventoryservice/InventoryServiceApplication.java
@@ -1,7 +1,11 @@
 package com.shopproject.inventoryservice;
 
+import com.shopproject.inventoryservice.model.Inventory;
+import com.shopproject.inventoryservice.repositary.InventoryRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class InventoryServiceApplication {
@@ -10,4 +14,19 @@ public class InventoryServiceApplication {
 		SpringApplication.run(InventoryServiceApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner loadData(InventoryRepository inventoryRepository) {
+		return  args -> {
+			Inventory inventory = new Inventory();
+			inventory.setSkuCode("iphone");
+			inventory.setQuantity(100);
+
+			Inventory inventory1 = new Inventory();
+			inventory1.setSkuCode("iphone1");
+			inventory1.setQuantity(0);
+
+			inventoryRepository.save(inventory);
+			inventoryRepository.save(inventory1);
+		};
+	}
 }
